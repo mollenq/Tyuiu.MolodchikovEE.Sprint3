6d7d7c749aeb287863c5d21fa8a3be396af702de
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-23 22:14:53 +0500

    Task6

---
+++ Tyuiu.MolodchikovEE.Sprint3.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolodchikovEE.Sprint3.Task6.V1.Lib
+{
+    public class DataService : ISprint3Task6V1
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int result = 0;
+            int x;
+
+            for (x = startValue; x <= stopValue; x++) 
+            {
+                for (int d = 1; d <= x; d++) 
+                {
+                    if (x % d == 0) 
+                    {
+                        result += d;
+                    }
+                }    
+            
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.MolodchikovEE.Sprint3.Task6.V1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MolodchikovEE.Sprint3.Task6.V1.Lib;
+namespace Tyuiu.MolodchikovEE.Sprint3.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Обработка целочисленной информации                                      *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                            *");
+            Console.WriteLine("* Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Напишите программу, которая ищет среди целых чисел, принадлежащих числово*");
+            Console.WriteLine("*ому отрезку [11, 17] сумму всех делителей.                               *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            var result = ds.GetSumTheDivisors(x,y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
