72d23c81cdfe654db4ff5ff2b8d6ef95024b34ae
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-23 20:19:07 +0500

    Task3

---
+++ Tyuiu.MolodchikovEE.Sprint3.Task3.V26.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolodchikovEE.Sprint3.Task3.V26.Lib
+{
+    public class DataService : ISprint3Task3V26
+    {
+        public int GetCharCount(string value, char item)
+        {
+            int count = 0;
+            foreach (char chr in value)
+            {
+                if (chr == item)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.MolodchikovEE.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MolodchikovEE.Sprint3.Task3.V26.Lib;
+namespace Tyuiu.MolodchikovEE.Sprint3.Task3.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла foreach                                                  *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Используя цикл foreach подсчитать количество букв “e” в строке: have a ni*");
+            Console.WriteLine("*ice time                                                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            string x = "have a nice time";
+            char y = 'e';
+
+
+            double result = ds.GetCharCount(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
