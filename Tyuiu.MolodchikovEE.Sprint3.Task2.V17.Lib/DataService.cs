9696d9e76b07e318f1d1688de0668c09baa00858
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-23 19:57:59 +0500

    Task2

---
+++ Tyuiu.MolodchikovEE.Sprint3.Task2.V17.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MolodchikovEE.Sprint3.Task2.V17.Lib
+{
+    public class DataService : ISprint3Task2V17
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double result = 0;
+            do
+            {
+                result = result + (Math.Pow(1.0/((Math.Cos(startValue)+2)),2));
+                startValue++;
+            } while (startValue <= stopValue);
+
+            return Math.Round(result,3);
+        }
+    }
+}
---
+++ Tyuiu.MolodchikovEE.Sprint3.Task2.V17/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.MolodchikovEE.Sprint3.Task2.V17.Lib;
+namespace Tyuiu.MolodchikovEE.Sprint3.Task2.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла do-while                                                 *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Молодчиков Егор Евгеньевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу используя цикл do...while, которая вычисляет сумму ряд*");
+            Console.WriteLine("*да по формуле                                                            *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            double result = ds.GetSumSeries(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
