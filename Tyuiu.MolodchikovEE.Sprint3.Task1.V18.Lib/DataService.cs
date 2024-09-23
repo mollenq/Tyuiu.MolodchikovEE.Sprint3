9d9ae62a53c81c4627a058dcc11211728166b21d
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-23 19:34:55 +0500

    Task1

--- Tyuiu.MolodchikovEE.Sprint3.Task1.V18.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint3.Task1.V18.Lib/DataService.cs
@@ -6,15 +6,16 @@ namespace Tyuiu.MolodchikovEE.Sprint3.Task1.V18.Lib
     {
         public double GetSumSeries(int startValue, int stopValue)
         {
-            double res = 0;
+            double S = 0;
 
             while (startValue <= stopValue) 
             {
-                res = res + ((Math.Sin(startValue) * (Math.Pow((1 / 4), 2))));
+                S = S + ((Math.Sin(startValue) * (Math.Pow((1.0 / 4), 2))));
 
                 startValue++;
 
-            } return Math.Round(res,3);
+            } 
+            return Math.Round(S,3);
         }
     }
 }
