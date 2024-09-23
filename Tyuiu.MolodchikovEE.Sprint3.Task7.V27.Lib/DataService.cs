de003285cee9be6413c828479cd0891fea8ea706
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-23 22:43:11 +0500

    Task7

--- Tyuiu.MolodchikovEE.Sprint3.Task7.V27.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint3.Task7.V27.Lib/DataService.cs
@@ -4,11 +4,11 @@ namespace Tyuiu.MolodchikovEE.Sprint3.Task7.V27.Lib
 {
     public class DataService : ISprint3Task7V27
     {
-        public double[] GetMassFunction(int startValue, int stopValue)
+        public object[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] result;
+            object[] result;
             int len = (stopValue - startValue) + 1;
-            result = new double[len];
+            result = new object[len];
             double y;
             int count = 0;
             for (int x = startValue; x <= stopValue; x++) 
